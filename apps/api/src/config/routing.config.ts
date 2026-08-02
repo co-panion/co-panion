480ee7e2d49441aa13054da7693815b1e28350ea
LucysLog <sunshower1051@outlook.com>
2026-08-03 01:33:05 +0900

    refactor(workspace): isolate shared modules

--- apps/api/src/config/routing.config.ts
+++ apps/api/src/config/routing.config.ts
@@ -1,7 +1,7 @@
 // API 버저닝(Versioning), 전역 인터셉터(Interceptor) // 하위 호환성 유지와 응답 데이터 규격화
 
 import { INestApplication, VersioningType } from '@nestjs/common';
-import { DEFAULT_API_VERSION } from '../common/constants/api.constant';
+import { DEFAULT_API_VERSION } from '@co-panion/shared';
 
 export const setupRouting = (app: INestApplication) => {
   // 글로벌 접두사 설정
